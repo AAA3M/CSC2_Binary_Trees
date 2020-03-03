23a453609815926a86067cc24dd03a8554f82783
AAA3M <alaric.mcgregoraaa@gmail.com>
2020-03-03 21:53:17 +0200

    Final Sample Data, (fixed innacurate data input)

--- bin/TestValues.py
+++ bin/TestValues.py
@@ -2,10 +2,10 @@ import os
 import subprocess
 
 def addTestValuesBST():
-    path = "/home/alaric/Documents/CSC2/Ass1/CSC2_Binary_Trees/DataFiles/LSBSTApp_Test_Samples"
+    path = "/home/alaric/Documents/CSC2/Ass1/CSC2_Binary_Trees/DataFiles/LSArrayApp_Test_Samples"
     filew = open(path, "a")
 
-    s = subprocess.check_output("""java LSBSTApp "DataFile.txt" """, shell=True)
+    s = subprocess.check_output("""java LSArrayApp "DataFile.txt" """, shell=True)
     x = (s.decode("utf-8"))
     x += "\n--------------------\n"
     filew.write(x)
